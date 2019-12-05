679da3e94cf497a1c5d12d05f9443314a4ae8f2c
VictorGrajales9 <victorgrajales9@hotmail.com>
2019-12-05 18:37:18 -0500

    se termino de organizar el admin

--- users/admin.py
+++ users/admin.py
@@ -53,3 +53,19 @@ class ProfileInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'profile'
 
+class UserAdmin(BaseUserAdmin):
+    
+    inlines = (ProfileInline,)
+    list_display= (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+        )
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
+        
+
